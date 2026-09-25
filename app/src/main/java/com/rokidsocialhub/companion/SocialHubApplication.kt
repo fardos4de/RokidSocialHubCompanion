1cd87e43c648e864eb2a7dc83bea6b9380bf4da7
fardos4de <fardos4de@gmail.com>
2026-09-25 22:17:04 +0200

    Initialize diagnostic logging before Rokid SDK

--- app/src/main/java/com/rokidsocialhub/companion/SocialHubApplication.kt
+++ app/src/main/java/com/rokidsocialhub/companion/SocialHubApplication.kt
@@ -5,6 +5,9 @@ import android.app.Application
 class SocialHubApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        DiagnosticLog.initialize(this)
+        DiagnosticLog.log("Application", "Social Hub process started")
+        DiagnosticLog.logEnvironmentSnapshot(this, "application_start")
         RokidTransport.initialize(this)
     }
 }
